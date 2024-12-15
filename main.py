96c736a7598e162616841ef04d43e847030aec09
caylus972 <pamigeiadigwe@gmail.com>
2024-12-15 17:01:10 +0000

    third comit

---
+++ main.py
@@ -0,0 +1,11 @@
+#imput a word
+text=str(input("enter a string :"))
+
+#reverse string
+#using step value as -1 to literate in reverse
+revText = text[::-1]
+text = revText
+
+
+print("reverse of given string is:")
+print(text)
